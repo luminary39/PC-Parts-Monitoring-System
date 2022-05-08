8b7e8d57847ed3d2949e2ab744d4cde4ea50a53b
Sakib714 <sakib39.official@gmail.com>
2022-05-09 01:28:52 +0600

    PSU part added

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-from xml.etree.ElementTree import TreeBuilder
-import ryans
-import star_tech
-import sky_land
-import netstar
-from datetime import date, datetime
 import os
-
+from datetime import date
+import netstar
+import ryans
+import sky_land
+import star_tech
 
 # File name
 
@@ -27,20 +25,21 @@ sky_land_cpu_price = sky_land.sky_land_price_check(
 
 cpu_average_price = (int(ryans_cpu_price) + int(start_tech_cpu_price) + int(sky_land_cpu_price)) // 3
 
-
-
 # Ram Parts
 
-ryans_ram_prise = ryans.ryans_price_check("https://www.ryanscomputers.com/corsair-vengeance-lpx-8gb-ddr4-3200mhz-desktop-ram")
+ryans_ram_prise = ryans.ryans_price_check(
+    "https://www.ryanscomputers.com/corsair-vengeance-lpx-8gb-ddr4-3200mhz-desktop-ram")
 
 star_tech_ram_prise = star_tech.star_tech_price_check("https://www.startech.com.bd/corsair-vengeance-lpx-8gb-3200mhz")
 
-sky_land_ram_prise = sky_land.sky_land_price_check("https://www.skyland.com.bd/product/corsair-vengeance-lpx-8gb-3200mhz-ram/")
+sky_land_ram_prise = sky_land.sky_land_price_check(
+    "https://www.skyland.com.bd/product/corsair-vengeance-lpx-8gb-3200mhz-ram/")
 
-netstar_ram_prise = netstar.netstar_price_check("https://www.netstar.com.bd/product/corsair-vengeance-lpx-8gb-3200mhz-ddr4-desktop-ram/")
+netstar_ram_prise = netstar.netstar_price_check(
+    "https://www.netstar.com.bd/product/corsair-vengeance-lpx-8gb-3200mhz-ddr4-desktop-ram/")
 
-ram_average_prise = (int(ryans_ram_prise) + int(star_tech_ram_prise) + int(sky_land_ram_prise) + int(netstar_ram_prise)) // 4
-
+ram_average_prise = (int(ryans_ram_prise) + int(star_tech_ram_prise) + int(sky_land_ram_prise) + int(
+    netstar_ram_prise)) // 4
 
 # SSD Part
 
@@ -52,86 +51,108 @@ sky_land_ssd_prise = sky_land.sky_land_price_check("https://www.skyland.com.bd/p
 
 netstar_ssd_prise = netstar.netstar_price_check("https://www.netstar.com.bd/product/team-mp33-256gb-m-2-pcie-ssd/")
 
-ssd_average_prise = (int(ryans_ssd_prise)+ int(star_tech_ssd_prise)+ int(sky_land_ssd_prise)+ int(netstar_ssd_prise))//4
-
-
+ssd_average_prise = (int(ryans_ssd_prise) + int(star_tech_ssd_prise) + int(sky_land_ssd_prise) + int(
+    netstar_ssd_prise)) // 4
 
 # HDD Part
 
-ryans_hdd_prise = ryans.ryans_price_check("https://www.ryanscomputers.com/seagate-barracuda-1tb-35-inch-sata-7200rpm-desktop-hdd-st1000dm010")
+ryans_hdd_prise = ryans.ryans_price_check(
+    "https://www.ryanscomputers.com/seagate-barracuda-1tb-35-inch-sata-7200rpm-desktop-hdd-st1000dm010")
 
 star_tech_hdd_prise = star_tech.star_tech_price_check("https://www.startech.com.bd/seagate-1tb-barracuda")
 
-sky_land_hdd_prise = sky_land.sky_land_price_check("https://www.skyland.com.bd/product/seagate-internal-1tb-sata-barracuda-hdd/")
+sky_land_hdd_prise = sky_land.sky_land_price_check(
+    "https://www.skyland.com.bd/product/seagate-internal-1tb-sata-barracuda-hdd/")
 
 netstar_hdd_prise = netstar.netstar_price_check("https://www.netstar.com.bd/404")
 
-hdd_average_prise = (int(ryans_hdd_prise)+ int(sky_land_hdd_prise)) //2
-
+hdd_average_prise = (int(ryans_hdd_prise) + int(sky_land_hdd_prise)) // 2
 
 # Motherboard part
 
-ryans_mb_prise = ryans.ryans_price_check("https://www.ryanscomputers.com/gigabyte-b660m-ds3h-ax-wi-fi-6-12th-gen-intel-motherboard")
+ryans_mb_prise = ryans.ryans_price_check(
+    "https://www.ryanscomputers.com/gigabyte-b660m-ds3h-ax-wi-fi-6-12th-gen-intel-motherboard")
 
-star_tech_mb_prise = star_tech.star_tech_price_check("https://www.startech.com.bd/gigabyte-b660m-ds3h-ax-ddr4-motherboard")
+star_tech_mb_prise = star_tech.star_tech_price_check(
+    "https://www.startech.com.bd/gigabyte-b660m-ds3h-ax-ddr4-motherboard")
 
-sky_land_mb_prise  = sky_land.sky_land_price_check("https://www.skyland.com.bd/product/gigabyte-b660m-ds3h-ax-ddr4-motherboard/")
+sky_land_mb_prise = sky_land.sky_land_price_check(
+    "https://www.skyland.com.bd/product/gigabyte-b660m-ds3h-ax-ddr4-motherboard/")
 
 netstar_mb_prise = netstar.netstar_price_check("https://www.netstar.com.bd/404")
 
-mb_average_prise = (int(ryans_mb_prise)+ int(star_tech_mb_prise)+ int(sky_land_mb_prise)) //3
+mb_average_prise = (int(ryans_mb_prise) + int(star_tech_mb_prise) + int(sky_land_mb_prise)) // 3
+
+# Powersupply Unit
+
+ryans_psu_prise = ryans.ryans_price_check(
+    "https://www.ryanscomputers.com/antec-caprum-strike-series-csk550-power-supply")
+
+star_tech_psu_prise = star_tech.star_tech_price_check(
+    "https://www.startech.com.bd/antec-cuprum-strike-csk-550w-power-supply")
+
+sky_land_psu_prise = sky_land.sky_land_price_check(
+    "https://www.skyland.com.bd/product/antec-cuprum-strike-csk-550w-power-supply/")
+
+netstar_psu_prise = netstar.netstar_price_check("https://www.netstar.com.bd/404")
+
+psu_average_prise = (int(ryans_psu_prise) + int(star_tech_psu_prise) + int(sky_land_psu_prise)) // 3
 
 
 
 
 
-report_string = f"              CPU         \n"\
+report_string = f"              CPU         \n" \
                 f"Intel 12th Gen Core i5-12400 Alder Lake Processor\n" \
                 f"******************************\n" \
                 f"Ryans: {ryans_cpu_price}\n" \
                 f"Star Tech: {start_tech_cpu_price}\n" \
                 f"Sky Land: {sky_land_cpu_price}\n" \
                 f"Net Star: Not Available Currently!\n\n" \
-                f"Average Price: {cpu_average_price} BDT\n\n\n"\
-                f"**************************************************\n\n\n"\
-                f"          RAM         \n"\
-                f"Corsair Vengeance LPX 8GB DDR4 3200MHz Desktop RAM\n"\
+                f"Average Price: {cpu_average_price} BDT\n\n\n" \
+                f"**************************************************\n\n\n" \
+                f"          RAM         \n" \
+                f"Corsair Vengeance LPX 8GB DDR4 3200MHz Desktop RAM\n" \
                 f"******************************\n" \
-                f"Ryans: {ryans_ram_prise}\n"\
-                f"Star Tech: {star_tech_ram_prise}\n"\
-                f"Sky Land: {sky_land_ram_prise}\n"\
-                f"Netstar: {netstar_ram_prise}\n\n"\
-                f"Average RAM Prise {ram_average_prise}\n\n\n"\
-                f"**************************************************\n\n\n"\
-                f"          SSD         \n"\
-                f"Team MP33 256GB M.2 PCIe SSD\n"\
+                f"Ryans: {ryans_ram_prise}\n" \
+                f"Star Tech: {star_tech_ram_prise}\n" \
+                f"Sky Land: {sky_land_ram_prise}\n" \
+                f"Netstar: {netstar_ram_prise}\n\n" \
+                f"Average RAM Prise {ram_average_prise}\n\n\n" \
+                f"**************************************************\n\n\n" \
+                f"          SSD         \n" \
+                f"Team MP33 256GB M.2 PCIe SSD\n" \
                 f"******************************\n" \
-                f"Ryans: {ryans_ssd_prise}\n"\
-                f"Star Tech: {star_tech_ssd_prise}\n"\
-                f"Sky Land: {sky_land_ssd_prise}\n"\
-                f"Netstar: {netstar_ssd_prise}\n\n"\
-                f"Average RAM Prise {ssd_average_prise}\n\n\n"\
-                f"**************************************************\n\n\n"\
-                f"          HDD         \n"\
-                f"Seagate Barracuda 7200RPM 1TB Desktop Hard disk(Internal)\n"\
+                f"Ryans: {ryans_ssd_prise}\n" \
+                f"Star Tech: {star_tech_ssd_prise}\n" \
+                f"Sky Land: {sky_land_ssd_prise}\n" \
+                f"Netstar: {netstar_ssd_prise}\n\n" \
+                f"Average RAM Prise {ssd_average_prise}\n\n\n" \
+                f"**************************************************\n\n\n" \
+                f"          HDD         \n" \
+                f"Seagate Barracuda 7200RPM 1TB Desktop Hard disk(Internal)\n" \
                 f"******************************\n" \
-                f"Ryans: {ryans_hdd_prise}\n"\
-                f"Star Tech: {star_tech_hdd_prise} [BUG Error]!\n"\
-                f"Sky Land: {sky_land_hdd_prise}\n"\
-                f"Netstar: {netstar_hdd_prise}\n\n"\
-                f"Average RAM Prise {hdd_average_prise}\n\n\n"\
-                f"          Motherboard         \n"\
-                f"Gigabyte B660M DS3H AX DDR4 12th Gen Micro ATX Motherboard\n"\
+                f"Ryans: {ryans_hdd_prise}\n" \
+                f"Star Tech: {star_tech_hdd_prise} [BUG Error]!\n" \
+                f"Sky Land: {sky_land_hdd_prise}\n" \
+                f"Netstar: {netstar_hdd_prise}\n\n" \
+                f"Average RAM Prise {hdd_average_prise}\n\n\n" \
+                f"          Motherboard         \n" \
+                f"Gigabyte B660M DS3H AX DDR4 12th Gen Micro ATX Motherboard\n" \
                 f"******************************\n" \
-                f"Ryans: {ryans_mb_prise}\n"\
-                f"Star Tech: {star_tech_mb_prise}\n"\
-                f"Sky Land: {sky_land_mb_prise}\n"\
-                f"Netstar: {netstar_mb_prise}\n\n"\
-                f"Average RAM Prise {mb_average_prise}\n\n\n"\
-
-
-
-
+                f"Ryans: {ryans_mb_prise}\n" \
+                f"Star Tech: {star_tech_mb_prise}\n" \
+                f"Sky Land: {sky_land_mb_prise}\n" \
+                f"Netstar: {netstar_mb_prise}\n\n" \
+                f"Average RAM Prise {mb_average_prise}\n\n\n" \
+                f"          Power Supply         \n" \
+                f"Antec Caprum Strike Series CSK550 550W Non Modular Power Supply\n" \
+                f"******************************\n" \
+                f"Ryans: {ryans_psu_prise}\n" \
+                f"Star Tech: {star_tech_psu_prise}\n" \
+                f"Sky Land: {sky_land_psu_prise}\n" \
+                f"Netstar: {netstar_psu_prise}\n\n" \
+                f"Average RAM Prise {psu_average_prise}\n\n\n" \
 
 # Saving File
 
